74e70b59b8477f1ac34af5120db2cf1f82d36fd1
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2023-10-16 15:04:32 -0300

    Criando classe pessoa

--- classe/Pessoa.php
+++ classe/Pessoa.php
@@ -9,5 +9,12 @@ class Pessoa{
 
     public function cadastrar(){
         $cx = (new DataBase())->getCx();
+        $cmdSql = 'INSERT INTO pessoa(email, nome, telefone, foto) VALUES (:email, :nome, :telefone, :foto)';
+        $cx_preparado = $cx->prepare($cmdSql);
+        $cx_preparado->bindValue(':email',$this->email);
+        $cx_preparado->bindValue(':nome',$this->nome);
+        $cx_preparado->bindValue(':telefone',$this->telefone);
+        $cx_preparado->bindValue(':foto',$this->foto);
+        return $cx_preparado->execute();
     }
 }
--- index.php
+++ index.php
@@ -1,13 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="txtNome" placeholder="nome">
+        <br>
+        <input type="text" name="txtEmail" placeholder="email">
+        <br>
+        <input type="text" name="txtTelefone" placeholder="telefone">
+        <br>
+        <input type="text" name="txtFoto" placeholder="foto">
+        <br>
+        <input type="submit" name="btnCadastrar">
+    </form>
+</body>
+</html>
 <?php
-    require_once ('classe/DataBase.php');
-    $db = new DataBase();
-
-    $cx = $db->getCx();
-
-    $cx_preparada = $cx->prepare("SELECT * FROM pessoa");
-    $cx_preparada->execute();
-
-    if($cx_preparada->rowCount() > 0){
-        $dados = $cx_preparada->fetchAll();
-        var_dump($dados);
+    if(isset($_POST['btnCadastrar'])){
+        require_once ('classe/Pessoa.php');
+        $pessoa = new Pessoa();
+        $pessoa->nome       = $_POST['txtNome'];
+        $pessoa->email      = $_POST['txtEmail'];
+        $pessoa->telefone   = $_POST['txtTelefone'];
+        $pessoa->foto       = $_POST['txtFoto'];
+    
+        var_dump($pessoa->cadastrar());
     }
