45db0645ff7003c0d786c45958a8a627c6de4d9d
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2023-10-23 12:54:30 -0300

    Base para a aula do dia 23/10

--- index.php
+++ index.php
@@ -6,27 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
-        <input type="text" name="txtNome" placeholder="nome">
-        <br>
-        <input type="text" name="txtEmail" placeholder="email">
-        <br>
-        <input type="text" name="txtTelefone" placeholder="telefone">
-        <br>
-        <input type="text" name="txtFoto" placeholder="foto">
-        <br>
-        <input type="submit" name="btnCadastrar">
-    </form>
+    
 </body>
 </html>
-<?php
-    if(isset($_POST['btnCadastrar'])){
-        require_once ('classe/Pessoa.php');
-        $pessoa = new Pessoa();
-        $pessoa->nome       = $_POST['txtNome'];
-        $pessoa->email      = $_POST['txtEmail'];
-        $pessoa->telefone   = $_POST['txtTelefone'];
-        $pessoa->foto       = $_POST['txtFoto'];
-    
-        var_dump($pessoa->cadastrar());
-    }
